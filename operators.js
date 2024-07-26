663c9b4ab9416ca76e2624d59ae5e377348dd4db
Lavanya Rapelli <lavanyarapelli555@gmail.com>
2024-07-26 17:55:36 +0530

    Day2_Operators

---
+++ operators.js
@@ -0,0 +1,155 @@
+// Agenda
+// Mathematical operator
+// comparisional operator
+
+// Mathematical operator
+
+// Additional operator(+)
+// subtractional operator(-)
+//exponential operator (**)
+// mathematical operator(*)
+// divisonal operator (/)
+
+// comparisional operator
+
+// less than < 
+// greater than > 
+// less than or equal to <=
+// greater than or equalto >= 
+
+// examples:
+
+let xx = 10
+let yy = 20
+
+console.log(x+y) 
+
+// additional operator
+
+// let x = 10
+// let y = 20
+
+// console.log(x-y) 
+
+// subtractional operator
+
+// let x = 10
+// let y = 20
+// console.log(x*y)
+
+// mathematical  operator
+
+// let x = 10
+// let y = 20
+
+// console.log(x/y)
+
+// divisional operator
+
+// ciel - go high 
+//  floor - go low
+
+// ex: 1.02 
+// floor - 1
+// ciel - 2
+
+// ex: -7.89
+// floor - -8
+// ciel - -7
+
+// let num = 5.39;
+
+// console.log(Math.floor((num)));
+// console.log(Math.ceil((num)));
+
+// this is for positive numbers
+
+// let num = -5.39;
+
+// console.log(Math.floor((num)));
+// console.log(Math.ceil((num)));
+// this is for negative numbers
+
+// Area of a circle = pi*r2
+
+// let radius = 7;
+// let area = 3.14 * (radius * radius);
+
+// console.log(area);
+
+
+// let radius = 7;
+// let area = Math.PI * (radius * radius);
+
+// console.log(area);
+
+// we can use direct functions also
+
+// Area of a rectangle = lb
+
+// let length = 8;
+// let breadth = 10;
+
+// console.log(length*breadth);
+
+// console.log(3>5);
+// console.log(13>5);
+
+// console.log(3>=3);
+// console.log(3<=3);
+
+// let ramAge = 20;
+// let shyamAge = 30;
+
+// console.log(ramAge > shyamAge);
+
+// let ramScore = 30;
+// let passingMarks = 35;
+
+// let isPassing =(ramScore >= passingMarks); 
+
+// console.log(isPassing);
+
+// let totalBill = 700;
+// let minFreeDelVal = 500;
+
+// let isEligibleForFreeDel = (totalBill >= minFreeDelVal)
+
+// console.log(isEligibleForFreeDel);
+
+// comparisional operators < > <= >= 
+
+// let is the good way to declare a variable 
+
+
+// two types of equality checkers are there in java 
+// == --> loose 
+// === --> strict 
+
+
+// console.log(5 == 5);
+
+// console.log("5" == 5);
+// data type for left side is a string 
+//  right side is number
+
+//  big brother only checks either the value or the data type
+
+// console.log("5" === 5);
+// data type for left side is a string 
+//  right side is number
+
+// parent will checks both value and data type 
+
+// console.log(5 ===5);
+
+// console.log("masai" == "Masai")
+// console.log("masai" != "Masai")
+// console.log("masai" !== "Masai")
+
+// console.log(1 == true);
+//using typecasting booloan will convert as 1 value so it gives true 
+// console.log(1 === true);
+// here it wont convert it will takes 1 as num and true as boolean so it will gives false 
+
+
